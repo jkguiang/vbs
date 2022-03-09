0d24484a898d816017e87e1d7c867cc4c74b7cd2
Jonathan Guiang <jkguiang@gmail.com>
2022-03-08 18:25:55 -0800

    added cutflow object for adding cutflows together

---
+++ tools/cutflow.py
@@ -0,0 +1,172 @@
+class Cut:
+    def __init__(self, name, parent=None, left=None, right=None, 
+                 n_pass=0, n_pass_weighted=0., n_fail=0, n_fail_weighted=0.):
+        self.name = name
+        self.n_pass = n_pass
+        self.n_pass_weighted = n_pass_weighted
+        self.n_fail = n_fail
+        self.n_fail_weighted = n_fail_weighted
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+    def __eq__(self, other_cut):
+        # FIXME: doesn't work when parent, left, or right is NoneType
+        same_parent_name = (self.parent.name == other_cut.parent.name)
+        same_left_name = (self.left.name == other_cut.left.name)
+        same_right_name = (self.right.name == other_cut.right.name)
+        same_lineage = (same_parent_name and same_left_name and same_right_name)
+        same_name = (self.name == other_cut.name)
+        return same_name and same_lineage
+
+    def __add__(self, other_cut):
+        if self == other_cut:
+            cut_sum = Cut(
+                name,
+                n_pass=(cut.n_pass + other_cut.n_pass),
+                n_pass_weighted=(cut.n_pass_weighted + other_cut.n_pass_weighted)
+            )
+            return cut_sum
+        else:
+            raise ValueError("can only add equivalent cuts")
+
+class Cutflow:
+    def __init__(self):
+        self.__cuts = {}
+        self.__root_cut_name = None
+        self.__terminal_cut_names = []
+        self.__cut_network = {}
+
+    def __getitem__(self, name):
+        return self.__cuts[name]
+
+    def __eq__(self, other_cutflow):
+        return self.__cut_network == other_cutflow.__cut_network
+
+    def __add__(self, other_cutflow):
+        if self != other_cutflow:
+            raise ValueError("can only add equivalent cutflows")
+        else:
+            added_cuts = {}
+            for name, cut in self.items():
+                other_cut = other_cutflow[name]
+                summed_cuts[name] = cut + other_cut
+            return Cutflow(cuts=summed_cuts, cut_network=self.__cut_network)
+
+    def __recursive_print(self, cut, tabs=""):
+        if cut is self.root_cut():
+            prefix = ""
+        elif cut is cut.parent.left:
+            if not cut.parent.right:
+                prefix = f"{tabs}\u2514\u2612\u2500"
+                tabs += "    "
+            else:
+                prefix = f"{tabs}\u251C\u2612\u2500"
+                tabs += "\u2502   "
+        elif cut is cut.parent.right:
+            prefix = f"{tabs}\u2514\u2611\u2500"
+            tabs += "    "
+
+        print(f"{prefix}{cut.name}")
+        print(f"{tabs}pass: {cut.n_pass} (raw) {cut.n_pass_weighted} (wgt)")
+        print(f"{tabs}fail: {cut.n_fail} (raw) {cut.n_fail_weighted} (wgt)")
+
+        if cut.left:
+            self.__recursive_print(cut.left, tabs=tabs)
+        if cut.right:
+            self.__recursive_print(cut.right, tabs=tabs)
+
+    def print(self):
+        self.__recursive_print(self.root_cut())
+
+    def items(self):
+        return self.__cuts.items()
+
+    def cut_names(self):
+        return self.__cuts.keys()
+
+    def cuts(self):
+        return self.__cuts.values()
+
+    def terminal_cuts(self):
+        return [self.__cuts[name] for name in self.__terminal_cut_names]
+
+    def root_cut(self):
+        return self.__cuts[self.__root_cut_name]
+
+    @staticmethod
+    def from_network(cuts, cut_network):
+        cutflow = Cutflow()
+        # Check inputs
+        if not cuts and not cut_network:
+            raise ValueError("list of cuts and cut network both empty")
+        elif not cuts:
+            raise ValueError("list of cuts empty")
+        elif not cut_network:
+            raise ValueError("cut network empty")
+        elif not set(cuts) == set(cut_network.keys()):
+            raise ValueError("list of cuts does not match cut network")
+        # Build cutflow
+        cutflow.__cuts = cuts
+        for name, (parent_name, left_name, right_name) in cut_network.items():
+            if parent_name:
+                cutflow.__cuts[name].parent = cutflow.__cuts[parent_name]
+            else:
+                if cutflow.__root_cut_name:
+                    raise Exception("invalid cutflow - multiple root cuts")
+                else:
+                    cutflow.__root_cut_name = name
+            if left_name:
+                cutflow.__cuts[name].left = cutflow.__cuts[left_name]
+            if right_name:
+                cutflow.__cuts[name].right = cutflow.__cuts[right_name]
+            if not left_name and not right_name:
+                cutflow.__terminal_cut_names.append(name)
+        # Check cutflow and return it if it is healthy
+        if len(cutflow.__terminal_cut_names) == 0:
+            raise Exception("invalid cutflow - no terminal cuts")
+        elif not cutflow.__root_cut_name:
+            raise Exception("invalid cutflow - no root cut")
+        else:
+            return cutflow
+
+    @staticmethod
+    def from_file(cutflow_file, delimiter=","):
+        cuts = {}
+        cut_network = {}
+        with open(cutflow_file, "r") as f_in:
+            for line in f_in:
+                # Read cut attributes
+                line = line.replace("\n", "")
+                cut_attr = line.split(delimiter)
+                # Extract basic info
+                name = cut_attr[0]
+                n_pass, n_pass_weighted, n_fail, n_fail_weighted = cut_attr[1:5]
+                # Cast string counts into numbers
+                n_pass = int(n_pass)
+                n_fail = int(n_fail)
+                n_pass_weighted = float(n_pass_weighted)
+                n_fail_weighted = float(n_fail_weighted)
+                # Extract lineage
+                parent_name, left_name, right_name = cut_attr[5:]
+                # Cast 'null' values into NoneType
+                if parent_name == "null":
+                    parent_name = None
+                if left_name == "null":
+                    left_name = None
+                if right_name == "null":
+                    right_name = None
+                # Create new cut and update cut network
+                cuts[name] = Cut(
+                    name, 
+                    n_pass=n_pass, n_pass_weighted=n_pass_weighted, 
+                    n_fail=n_fail, n_fail_weighted=n_fail_weighted
+                )
+                cut_network[name] = (parent_name, left_name, right_name)
+
+        return Cutflow.from_network(cuts=cuts, cut_network=cut_network)
+
+if __name__ == "__main__":
+    zzz_cutflow = Cutflow.from_file("studies/pilot/output/2018/ZZZ_Cutflow.cflow")
+    wwz_cutflow = Cutflow.from_file("studies/pilot/output/2018/WWZ_4F_Cutflow.cflow")
+    (zzz_cutflow + wwz_cutflow).print()
