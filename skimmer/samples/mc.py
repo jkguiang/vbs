8dfe03b2b50acbb4b379acb56d25a16c40ed7804
Jonathan Guiang <jkguiang@gmail.com>
2022-06-09 12:58:37 -0700

    added more signal MC samples

--- skimmer/samples/mc.py
+++ skimmer/samples/mc.py
@@ -62,26 +62,26 @@ nanoaodv9_sig = [
         dataset="RunIISummer20UL18_VBSOSWWH_incl_C2V_4_Azure", 
         use_xrootd=True
     ),
-    DirectorySample(
-        location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN_v1", 
-        dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN", 
-        use_xrootd=True
-    ),
-    DirectorySample(
-        location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN_v1", 
-        dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN", 
-        use_xrootd=True
-    ),
-    DirectorySample(
-        location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN_v1", 
-        dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN", 
-        use_xrootd=True
-    ),
-    DirectorySample(
-        location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN_v1", 
-        dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN", 
-        use_xrootd=True
-    ),
+    # DirectorySample(
+    #     location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN_v1", 
+    #     dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN", 
+    #     use_xrootd=True
+    # ),
+    # DirectorySample(
+    #     location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN_v1", 
+    #     dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN", 
+    #     use_xrootd=True
+    # ),
+    # DirectorySample(
+    #     location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN_v1", 
+    #     dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN", 
+    #     use_xrootd=True
+    # ),
+    # DirectorySample(
+    #     location="/ceph/cms/store/user/jguiang/VBSWHSignalGeneration/v1/VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN_v1", 
+    #     dataset="VBSWH_mkW_Inclusive_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN", 
+    #     use_xrootd=True
+    # ),
 ]
 
 # DIS searches:
