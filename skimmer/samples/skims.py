7bab64eb8513494e2c5a9ae79f3491afca606791
Jonathan Guiang <jkguiang@gmail.com>
2022-06-17 15:51:53 -0700

    made names more clean

--- skimmer/samples/skims.py
+++ skimmer/samples/skims.py
@@ -1,6 +1,14 @@
 from metis.Sample import DirectorySample
 import glob
 
-nanoaodv9_1lep_1ak8_2ak4 = [
-    DirectorySample(location=d, dataset=d.split("/")[-1].split("_1lep_1ak8")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVHSkim/1lep_1ak8_2ak4_v1/*")        
+sig_1lep_1ak8_2ak4_v1 = [
+    DirectorySample(location=d, dataset=d.split("/")[-1].split("sig_1lep_1ak8")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVHSkim/sig_1lep_1ak8_2ak4_v1/*")
+]
+
+bkg_1lep_1ak8_2ak4_v1 = [
+    DirectorySample(location=d, dataset=d.split("/")[-1].split("bkg_1lep_1ak8")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVHSkim/bkg_1lep_1ak8_2ak4_v1/*")
+]
+
+data_1lep_1ak8_2ak4_v1 = [
+    DirectorySample(location=d, dataset=d.split("/")[-1].split("data_1lep_1ak8")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVHSkim/data_1lep_1ak8_2ak4_v1/*")
 ]
