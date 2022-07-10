5109dff67abb31e69d1e630b7e08287bdc0be986
Jonathan Guiang <jkguiang@gmail.com>
2022-07-09 22:13:41 -0700

    added efficiency plot

--- analysis/utils/analysis.py
+++ analysis/utils/analysis.py
@@ -2,8 +2,13 @@ import uproot
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from matplotlib import colors
+from matplotlib import pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 from matplotlib.colors import LogNorm, Normalize
 from mpl_toolkits.axes_grid1 import ImageGrid
+import scipy.stats
 
 def clip(np_array, bins):
     clip_low = 0.5 * (bins[0] + bins[1])
@@ -80,27 +85,197 @@ class PandasAnalysis:
 
     def sig_df(self, selection=None):
         if not selection:
-            return self.df[self.df.is_signal]
+            return self.df[~self.df.is_data & self.df.is_signal]
         else:
             if selection and type(selection) == str:
                 selection = self.df.eval(selection)
-            return self.df[self.df.is_signal & selection]
+            return self.df[~self.df.is_data & self.df.is_signal & selection]
 
     def bkg_df(self, selection=None):
         if not selection:
-            return self.df[~self.df.is_signal]
+            return self.df[~self.df.is_data & ~self.df.is_signal]
         else:
             if selection and type(selection) == str:
                 selection = self.df.eval(selection)
-            return self.df[~self.df.is_signal & selection]
+            return self.df[~self.df.is_data & ~self.df.is_signal & selection]
 
     def data_df(self, selection=None):
         if not selection:
-            return self.df[~self.df.is_signal]
+            return self.df[self.df.is_data]
         else:
             if selection and type(selection) == str:
                 selection = self.df.eval(selection)
             return self.df[self.df.is_data & selection]
+
+    @staticmethod
+    def clopper_pearson_error(passed, total, level=0.6827):
+        """Matching TEfficiency::ClopperPearson()
+           stolen from Nick Amin: https://github.com/aminnj/yahist
+        """
+
+        alpha = 0.5 * (1.0 - level)
+        low = scipy.stats.beta.ppf(alpha, passed, total - passed + 1)
+        high = scipy.stats.beta.ppf(1 - alpha, passed + 1, total - passed)
+        return low, high
+
+    @staticmethod
+    def ratio_plot(numer, denom, bin_edges, numer_err=None, denom_err=None, xerr=0, yerr=0, 
+                   axes=None, label="", fmt="ko", markersize=3):
+        # Get bin dimensions
+        bin_widths = bin_edges[1:] - bin_edges[:-1]
+        bin_centers = bin_edges[:-1] + 0.5*bin_widths
+        # Aviod divide-by-zero
+        denom[denom == 0] = 10^(-8)
+        # Get ratios
+        ratios = 1.0*numer/denom
+        # Get errors on ratio
+        if not yerr:
+            bothzero = (numer == 0) & (denom == 0)
+            err_down, err_up = PandasAnalysis.clopper_pearson_error(numer, denom)
+            # these are actually the positions for down and up, but we want the errors
+            # wrt to the central value
+            err_up = err_up - ratios
+            err_down = ratios - err_down
+            errors = 0.5*(err_down + err_up)  # nominal errors are avg of up and down
+            # For consistency with TEfficiency, up error is 1 if we have 0/0
+            err_up[bothzero] = 1.0
+            yerr = errors
+        # Plot ratios
+        axes.errorbar(
+            bin_centers, 
+            ratios, 
+            xerr=xerr, 
+            yerr=yerr, 
+            fmt=fmt, 
+            label=label, 
+            markersize=markersize
+        )
+        # Get relative errors
+        if not type(numer_err) == type(None) and not type(denom_err) == type(None):
+            rel_errors = (
+                (numer_err/denom)**2.0 + (denom_err*numer/denom**2.0)**2.0
+            )**0.5
+            # Plot relative errors
+            err_points = np.repeat(bin_edges, 2)[1:-1]
+            err_high = np.repeat(1 + rel_errors, 2)
+            err_low = np.repeat(1 - rel_errors, 2)
+            axes.fill_between(
+                err_points, err_high, err_low, 
+                step="mid", 
+                hatch="///////", 
+                facecolor="none",
+                edgecolor=(0.75,0.75,0.75), 
+                linewidth=0.0, 
+                linestyle="-",
+                zorder=2
+            )
+        # Formatting
+        axes.margins(x=0) # Remove axis margins
+        
+        return
+
+    @staticmethod
+    def errorbar_hist(vals, weights=None, bins=None, yerr=None, fmt="ko", label="", 
+                      markersize=4, fig=None, axes=None):
+        if not fig or not axes:
+            fig = plt.figure(figsize=(6.4,4.8))
+            axes = plt.subplot(111)
+        # Get histogram values
+        counts, bin_edges = np.histogram(vals, weights=weights, bins=bins)
+        # Get bin dimensions
+        bin_widths = bin_edges[1:] - bin_edges[:-1]
+        bin_centers = bin_edges[:-1] + 0.5*bin_widths
+        # Plot error bars
+        axes.errorbar(
+            bin_centers, 
+            counts, 
+            yerr=yerr, 
+            fmt=fmt, 
+            label=label,
+            markersize=markersize
+        )
+        
+        return counts, bin_edges
+
+    @staticmethod
+    def efficiency_plot(numer_df, denom_df, column, denom_label="", numer_label="",
+                        numer_weights=None, denom_weights=None, ratio_label="", 
+                        x_label="", transf=lambda x: x, bins=None):
+
+        fig = plt.figure(figsize=(6.4*1.5,4.8*1.25*1.5))
+        gs = gridspec.GridSpec(ncols=1, nrows=2, figure=fig, height_ratios=[2,0.65], hspace=0.05)
+        hist_axes = fig.add_subplot(gs[0])
+        ratio_axes = fig.add_subplot(gs[1])
+
+        # Plot denominator
+        denom_counts, _, _ = hist_axes.hist(
+            clip(transf(denom_df[column]), bins), 
+            bins=bins,
+            color="yellow", 
+            histtype="stepfilled", 
+            edgecolor="k",
+            zorder=1,
+            label=denom_label,
+            weights=denom_weights
+        )
+        # Get denominator errors
+        denom_err = np.sqrt(denom_counts)
+        denom_err_points = np.repeat(bins, 2)[1:-1]
+        denom_err_high = np.repeat(denom_counts + denom_err, 2)
+        denom_err_low = np.repeat(denom_counts - denom_err, 2)
+        # Plot denominator errors
+        hist_axes.fill_between(
+            denom_err_points, 
+            denom_err_high, 
+            denom_err_low, 
+            step="mid", 
+            hatch="///////", 
+            facecolor="none",
+            edgecolor=(0.75,0.75,0.75), 
+            linewidth=0.0, 
+            linestyle="-",
+            zorder=2
+        );
+
+        # Plot numerator
+        numer_counts, _ = PandasAnalysis.errorbar_hist(
+            clip(transf(numer_df[column]), bins), 
+            bins=bins,
+            fig=fig, 
+            axes=hist_axes, 
+            label=numer_label,
+            weights=numer_weights
+        )
+        # Get numerator errors
+        numer_err = np.sqrt(numer_counts)
+        # Formatting
+        hist_axes.set_ylim(bottom=0)
+        hist_axes.legend(fontsize=14);
+        hist_axes.set_ylabel("Counts", size=18)
+        hist_axes.margins(x=0) # Remove axis margins
+        hist_axes.xaxis.set_minor_locator(AutoMinorLocator())
+        hist_axes.yaxis.set_minor_locator(AutoMinorLocator())
+        hist_axes.tick_params(labelbottom=False)
+
+        # Plot ratio
+        PandasAnalysis.ratio_plot(
+            numer_counts, 
+            denom_counts, 
+            bins, 
+            numer_err=numer_err, 
+            denom_err=denom_err, 
+            axes=ratio_axes
+        );
+        ratio_axes.axhline(y=1, color="k", linestyle="--", alpha=0.75, linewidth=0.75)
+        # Formatting
+        ratio_axes.xaxis.set_minor_locator(AutoMinorLocator())
+        ratio_axes.yaxis.set_minor_locator(AutoMinorLocator())
+        ratio_axes.set_ylabel(ratio_label, size=18);
+        ratio_axes.set_ylim([0.5,1.5]);
+        ratio_axes.set_xlim([bins[0], bins[-1]]);
+        ratio_axes.set_xlabel(x_label, size=18)
+        
+        return hist_axes, ratio_axes
 
 class Optimization(PandasAnalysis):
 
@@ -128,7 +303,7 @@ class Optimization(PandasAnalysis):
                 self.df.loc[split_right, f"{name}_weight"] *= orig_integral/split_right_integral
 
     def plot_sig_vs_bkg(self, column, bins, selection="", abs=False, raw=False, 
-                        xlabel="", logy=False):
+                        x_label="", logy=False):
 
         fig, axes = plt.subplots(figsize=(12, 9))
 
@@ -161,14 +336,14 @@ class Optimization(PandasAnalysis):
             log=logy
         )
         
-        axes.set_xlabel(xlabel, size=18)
+        axes.set_xlabel(x_label, size=18)
         axes.set_ylabel("Events", size=18)
         axes.legend(fontsize=16)
 
         return axes
 
     def plot_sig_vs_bkg_2D(self, xcolumn, ycolumn, xbins, ybins, selection="", xabs=False, 
-                           yabs=False, raw=False, xlabel="", ylabel="", logz=False):
+                           yabs=False, raw=False, x_label="", y_label="", logz=False):
 
         fig, axes = plt.subplots(1, 3, figsize=(24, 9), gridspec_kw={"width_ratios": [20, 20, 1]})
 
@@ -224,9 +399,9 @@ class Optimization(PandasAnalysis):
 
         fig.colorbar(sig_mesh, cax=axes[2])
         
-        axes[0].set_xlabel(xlabel, size=18)
-        axes[0].set_ylabel(ylabel, size=18)
-        axes[1].set_xlabel(xlabel, size=18)
+        axes[0].set_xlabel(x_label, size=18)
+        axes[0].set_ylabel(y_label, size=18)
+        axes[1].set_xlabel(x_label, size=18)
 
         return axes
 
@@ -262,3 +437,22 @@ class Validation(PandasAnalysis):
             return len(data_df), len(bkg_df)
         else:
             return data_df.event_weight.sum(), bkg_df.event_weight.sum()
+
+    def plot_data_vs_mc(self, column, bins, selection="", x_label="", logy=False, 
+                        transf=lambda x: x):
+
+        mc_df = self.bkg_df(selection=selection)
+        data_df = self.data_df(selection=selection)
+
+        PandasAnalysis.efficiency_plot(
+            data_df, 
+            mc_df, 
+            column, 
+            numer_label=f"total data [{len(data_df)} events]",
+            denom_label=f"total background [{mc_df.event_weight.sum():.1f} events]", 
+            denom_weights=mc_df.event_weight, 
+            ratio_label="data/mc", 
+            transf=transf,
+            x_label=x_label,
+            bins=bins
+        )
