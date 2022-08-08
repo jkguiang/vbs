ff8c0bb8a03571221bb797a777deea3076854def
Jonathan Guiang <jkguiang@gmail.com>
2022-08-08 10:39:42 -0700

    various updates

--- analysis/utils/analysis.py
+++ analysis/utils/analysis.py
@@ -1,26 +1,23 @@
+import os
 import uproot
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import gridspec
-from matplotlib import colors
 from matplotlib import pyplot as plt
+from matplotlib import gridspec
 from matplotlib.ticker import AutoMinorLocator
-from matplotlib.colors import LogNorm, Normalize
-from mpl_toolkits.axes_grid1 import ImageGrid
-import scipy.stats
 import yahist
 
 from utils.cutflow import Cut, Cutflow, CutflowCollection
 
 def clip(np_array, bins):
-    clip_low = 0.5 * (bins[0] + bins[1])
-    clip_high = 0.5 * (bins[-2] + bins[-1])
+    clip_low = 0.5*(bins[0] + bins[1])
+    clip_high = 0.5*(bins[-2] + bins[-1])
     return np.clip(np_array, clip_low, clip_high)
 
 class PandasAnalysis:
     def __init__(self, sig_root_files=None, bkg_root_files=None, data_root_files=None, 
-                 ttree_name="Events", weight_columns=None):
+                 ttree_name="Events", weight_columns=None, plots_dir=None):
+
         dfs = []
         # Load signal
         if sig_root_files:
@@ -33,9 +30,11 @@ class PandasAnalysis:
                     df["is_data"] = False
                     dfs.append(df)
         # Load background
+        bkg_names = []
         if bkg_root_files:
             for root_file in bkg_root_files:
                 name = root_file.split("/")[-1].replace(".root", "")
+                bkg_names.append(name)
                 with uproot.open(root_file) as f:
                     df = f.get(ttree_name).arrays(library="pd")
                     df["name"] = name
@@ -61,21 +60,38 @@ class PandasAnalysis:
             for weight_col in weight_columns[1:]:
                 self.df["event_weight"] *= self.df[weight_col]
 
+        self.plots_dir = plots_dir
+        if self.plots_dir:
+            os.makedirs(self.plots_dir, exist_ok=True)
+
         self.cutflows = CutflowCollection(
             cutflows={name: Cutflow() for name in self.df.name.unique()}
         )
         self.__update_cutflows("base")
 
-    def __update_cutflows(self, selection):
-        selection = selection.replace(">", "gt")
-        selection = selection.replace("<", "lt")
-        selection = selection.replace(">=", "geq")
-        selection = selection.replace("<=", "leq")
-        selection = selection.replace("==", "eq")
-        selection = selection.replace("!=", "neq")
-        selection = selection.replace(".", "p")
-        selection = selection.replace("-", "m")
+        if len(bkg_names) <= 10:
+            colors = [
+                "#364b9a", "#e8eff6", "#6ea5cd", "#97cae1", "#c2e4ee", 
+                "#eaeccc", "#feda8a", "#fdb366", "#f67e4b", "#dd3c2d"
+            ]
+            self.bkg_colors = {name: colors[i] for i, name in enumerate(bkg_names)}
+        else:
+            self.bkg_colors = {}
+
+    @staticmethod
+    def get_selection_str(selection):
         selection = selection.replace(" ", "_")
+        repl_map = {
+            ">=": "geq", "<=": "leq", ">": "gt", "<": "lt", "==": "eq", "!=": "neq",
+            ".": "p", "-": "m"
+        }
+        for char, repl in repl_map.items():
+            selection = selection.replace(char, repl)
+
+        return selection
+
+    def __update_cutflows(self, selection):
+        selection = PandasAnalysis.get_selection_str(selection)
         for name, cutflow in self.cutflows.items():
             # Get previous cut
             if len(cutflow) > 0:
@@ -99,10 +115,18 @@ class PandasAnalysis:
             else:
                 cutflow.set_root_cut(new_cut)
 
+        if "TotalBkg" in self.cutflows.names:
+            self.cutflows.pop("TotalBkg")
+
         self.cutflows["TotalBkg"] = self.cutflows.sum()
+
         for name in self.df[self.df.is_data | self.df.is_signal].name.unique():
             self.cutflows["TotalBkg"] -= self.cutflows[name]
 
+        self.cutflows.write_csv(
+            f"{self.plots_dir}/cutflow.csv",
+            cutflow.terminal_cut_names[-1]
+        )
 
     def print_cutflow(self):
         print("\n".join(self.cutflows.get_csv(self.cutflows.terminal_cut_names[-1])))
@@ -184,6 +208,9 @@ class PandasAnalysis:
         df = self.bkg_df(selection=selection)
         return np.sqrt(len(df))
 
+    def dump_plots(self, plots_yaml):
+        return
+
 class Optimization(PandasAnalysis):
 
     def get_event_counts(self, selection=None, raw=False):
@@ -226,7 +253,7 @@ class Optimization(PandasAnalysis):
 
         denom_df = self.bkg_df(selection=base_selection)
         denom = yahist.Hist1D(
-            denom_df[column],
+            transf(denom_df[column]),
             bins=bins, 
             weights=denom_df.event_weight
         );
@@ -238,7 +265,7 @@ class Optimization(PandasAnalysis):
         for selection in selections:
             numer_df = self.bkg_df(selection=f"({base_selection}) and ({selection})")
             numer = yahist.Hist1D(
-                numer_df[column],
+                transf(numer_df[column]),
                 bins=bins, 
                 weights=numer_df.event_weight
             );
@@ -248,15 +275,32 @@ class Optimization(PandasAnalysis):
             numer.plot(ax=hist_axes, label=f"after {selection}")
             (numer/denom).plot(ax=ratio_axes, errors=True)
 
+        hist_axes.xaxis.set_minor_locator(AutoMinorLocator())
+        hist_axes.yaxis.set_minor_locator(AutoMinorLocator())
         hist_axes.legend(fontsize=16)
+        hist_axes.set_xmargin(0)
         if norm:
             hist_axes.set_ylabel("a.u.", size=18)
         else:
             hist_axes.set_ylabel("Events", size=18)
 
+        ratio_axes.xaxis.set_minor_locator(AutoMinorLocator())
+        ratio_axes.yaxis.set_minor_locator(AutoMinorLocator())
+        ratio_axes.axhline(y=1, color="k", linestyle="--", alpha=0.75, linewidth=0.75)
         ratio_axes.set_xlabel(x_label, size=18)
         ratio_axes.set_ylabel("after/before", size=18)
         ratio_axes.set_ylim([0.5, 2.0])
+
+        if self.plots_dir:
+            plot_file = f"{self.plots_dir}/{column}_correlations.png"
+            with open(plot_file.replace("png", "txt"), "w") as plot_txt:
+                if base_selection:
+                    plot_file = plot_file.replace(".png", f"_{PandasAnalysis.get_selection_str(base_selection)}.png")
+                    plot_txt.write(f"BASE_SELECTION: {base_selection}\n")
+                plot_txt.write("SELECTIONS:\n")
+                plot_txt.write("\n".join(selections))
+
+            plt.savefig(plot_file)
 
         return hist_axes, ratio_axes
 
@@ -284,7 +328,8 @@ class Optimization(PandasAnalysis):
                     transf(bkg_df[bkg_df.name == name][column]),
                     bins=bins, 
                     weights=weights,
-                    label=f"{name} [{sum(weights):.1f} events]"
+                    label=f"{name} [{sum(weights):.1f} events]",
+                    color=self.bkg_colors[name] if self.bkg_colors else None
                 )
                 bkg_hists.append(hist)
 
@@ -303,22 +348,44 @@ class Optimization(PandasAnalysis):
             bins=bins, 
             weights=sig_weights,
             label=f"total signal [{sum(sig_weights):0.1f} events]",
-            color="r"
+            color="k"
         )
         if norm:
+            bkg_hist = bkg_hist.normalize()
             sig_hist = sig_hist.normalize()
 
         # Plot everything
         if bkg_hists:
-            bkg_hists.sort(key=lambda h: sum(h.counts), reverse=True)
-            yahist.utils.plot_stack(bkg_hists, ax=axes, histtype="stepfilled", alpha=0.75)
-        bkg_hist.plot(ax=axes, alpha=0.75)
+            bkg_hists.sort(key=lambda h: sum(h.counts))
+            yahist.utils.plot_stack(bkg_hists, ax=axes, histtype="stepfilled")
+        bkg_hist.plot(ax=axes, alpha=0.5)
         sig_hist.plot(ax=axes, linewidth=2)
         
+        axes.xaxis.set_minor_locator(AutoMinorLocator())
+        axes.yaxis.set_minor_locator(AutoMinorLocator())
         axes.set_xlabel(x_label, size=18)
-        axes.set_ylabel("Events", size=18)
         axes.set_ylim(bottom=0)
-        axes.legend(fontsize=14)
+        if stacked:
+            axes.legend(fontsize=14)
+        else:
+            axes.legend(fontsize=16)
+        if norm:
+            axes.set_ylabel("a.u.", size=18)
+        else:
+            axes.set_ylabel("Events", size=18)
+
+        if self.plots_dir:
+            plot_file = f"{self.plots_dir}/{column}_sig_vs_bkg.png"
+            if stacked:
+                plot_file = plot_file.replace(".png", "_stacked.png")
+            if norm:
+                plot_file = plot_file.replace(".png", "_norm.png")
+            if selection:
+                plot_file = plot_file.replace(".png", f"_{PandasAnalysis.get_selection_str(selection)}.png")
+                with open(plot_file.replace("png", "txt"), "w") as plot_txt:
+                    plot_txt.write(f"SELECTION: {selection}")
+
+            plt.savefig(plot_file)
 
         return axes
 
@@ -355,7 +422,7 @@ class Optimization(PandasAnalysis):
 class Validation(PandasAnalysis):
 
     def get_event_counts(self, selection=None, raw=False):
-        data_count = self.data_count(selection=selection, raw=raw)
+        data_count = self.data_count(selection=selection)
         mc_count = self.bkg_count(selection=selection, raw=raw)
         return data_count, mc_count
 
@@ -365,20 +432,21 @@ class Validation(PandasAnalysis):
         return data_error, mc_error
 
     def plot_data_vs_mc(self, column, bins, selection="", x_label="", logy=False, 
-                        transf=lambda x: x, norm=False):
+                        transf=lambda x: x, norm=False, stacked=False):
 
         fig = plt.figure(figsize=(6.4*1.5, 4.8*1.25*1.5))
         gs = gridspec.GridSpec(ncols=1, nrows=2, figure=fig, height_ratios=[2, 0.65], hspace=0.1)
         hist_axes = fig.add_subplot(gs[0])
         ratio_axes = fig.add_subplot(gs[1])
 
-        # Get numerator and denominator
+        # Get numerator (data) and denominator (bkg MC)
         denom_df = self.bkg_df(selection=selection)
         denom = yahist.Hist1D(
             transf(denom_df[column]),
             bins=bins, 
             weights=denom_df.event_weight,
-            label=f"MC [{denom_df.event_weight.sum():0.1f} events]"
+            label=f"MC [{denom_df.event_weight.sum():0.1f} events]",
+            color="k"
         );
         numer_df = self.data_df(selection=selection)
         numer = yahist.Hist1D(
@@ -392,6 +460,22 @@ class Validation(PandasAnalysis):
             numer = numer.normalize()
             denom = denom.normalize()
 
+        # Get stacked backgrounds
+        bkg_hists = []
+        if stacked:
+            for name in denom_df.name.unique():
+                weights = denom_df.event_weight[denom_df.name == name]
+                if norm:
+                    weights /= denom_df.event_weight.sum()
+                hist = yahist.Hist1D(
+                    transf(denom_df[denom_df.name == name][column]),
+                    bins=bins, 
+                    weights=weights,
+                    label=f"{name} [{sum(weights):.1f} events]",
+                    color=self.bkg_colors[name] if self.bkg_colors else None
+                )
+                bkg_hists.append(hist)
+
         # Get ratio
         ratio = numer/denom
         # Get ratio errors
@@ -400,11 +484,12 @@ class Validation(PandasAnalysis):
         denom_counts = denom.counts
         denom_counts[denom_counts == 0] = 1e-12
         rel_errors = np.sqrt(
-            (numer.errors/numer_counts)**2 + (denom.errors/denom_counts)**2
+            (numer.errors/denom_counts)**2 
+            + (denom.errors*numer.counts/denom.counts**2)**2
         )
 
         # Plot hists and ratio
-        denom.plot(ax=hist_axes)
+        denom.plot(ax=hist_axes, alpha=0.5)
         numer.plot(ax=hist_axes, errors=True)
         ratio.plot(ax=ratio_axes, errors=True, color="k")
         # Plot relative errors
@@ -422,9 +507,19 @@ class Validation(PandasAnalysis):
             zorder=2
         )
 
-        hist_axes.legend(fontsize=16)
+        # Plot stacked bkg hists
+        if bkg_hists:
+            bkg_hists.sort(key=lambda h: sum(h.counts))
+            yahist.utils.plot_stack(bkg_hists, ax=hist_axes, histtype="stepfilled")
+
+        if stacked:
+            hist_axes.legend(fontsize=14)
+        else:
+            hist_axes.legend(fontsize=16)
         hist_axes.xaxis.set_minor_locator(AutoMinorLocator())
         hist_axes.yaxis.set_minor_locator(AutoMinorLocator())
+        hist_axes.set_xmargin(0)
+        hist_axes.set_ylim(bottom=0)
         if norm:
             hist_axes.set_ylabel("a.u.", size=18)
         else:
@@ -436,6 +531,16 @@ class Validation(PandasAnalysis):
         ratio_axes.set_xlabel(x_label, size=18)
         ratio_axes.set_ylabel("data/MC", size=18)
         ratio_axes.set_ylim([0.5, 2.0])
+
+        if self.plots_dir:
+            plot_file = f"{self.plots_dir}/{column}_data_vs_mc.png"
+            if selection:
+                plot_file = plot_file.replace(".png", f"_{PandasAnalysis.get_selection_str(selection)}.png")
+                with open(plot_file.replace("png", "txt"), "w") as plot_txt:
+                    plot_txt.write(f"SELECTION: {selection}")
+
+            print(f"Wrote plot to {plot_file}")
+            plt.savefig(plot_file)
 
         return hist_axes, ratio_axes
 
@@ -451,7 +556,8 @@ class Extrapolation(PandasAnalysis):
         bkg_error = self.bkg_error(selection=selection, raw=raw)
         return sig_error, bkg_error
 
-    def ABCD(self, h_right, v_up, base_selection, h_dir="right", v_dir="up", label=""):
+    def ABCD(self, h_right, v_up, base_selection, h_dir="right", v_dir="up", label="",
+             show_raw=False, show_data=False):
         """
         Compute yields for an ABCD extrapolation in the following configuration:
 
@@ -459,10 +565,11 @@ class Extrapolation(PandasAnalysis):
             -------|-------
                A   |   B
 
+        where Region D is the signal region, and Regions A, B, and C are control regions.
         """
+        # Assemble regions
         h_left = f"(not ({h_right}))"
         v_down = f"(not ({v_up}))"
-        # Assemble regions
         if h_dir.lower() == "right" and v_dir.lower() == "up":
             A_sel = f"{base_selection} and {h_left} and {v_down}"
             B_sel = f"{base_selection} and {h_right} and {v_down}"
@@ -483,32 +590,77 @@ class Extrapolation(PandasAnalysis):
             B_sel = f"{base_selection} and {h_right} and {v_up}"
             C_sel = f"{base_selection} and {h_right} and {v_down}"
             D_sel = f"{base_selection} and {h_left} and {v_down}"
+
         # Get yields and errors for each region
         A_sig_wgt, A_bkg_wgt = self.get_event_counts(selection=A_sel)
-        A_sig_raw, A_bkg_raw = self.get_event_counts(selection=A_sel, raw=True)
         A_sig_err, A_bkg_err = self.get_event_errors(selection=A_sel)
         B_sig_wgt, B_bkg_wgt = self.get_event_counts(selection=B_sel)
-        B_sig_raw, B_bkg_raw = self.get_event_counts(selection=B_sel, raw=True)
         B_sig_err, B_bkg_err = self.get_event_errors(selection=B_sel)
         C_sig_wgt, C_bkg_wgt = self.get_event_counts(selection=C_sel)
-        C_sig_raw, C_bkg_raw = self.get_event_counts(selection=C_sel, raw=True)
         C_sig_err, C_bkg_err = self.get_event_errors(selection=C_sel)
         D_sig_wgt, D_bkg_wgt = self.get_event_counts(selection=D_sel)
-        D_sig_raw, D_bkg_raw = self.get_event_counts(selection=D_sel, raw=True)
         D_sig_err, D_bkg_err = self.get_event_errors(selection=D_sel)
-        # Calculate extrapolation factors
+        if show_raw:
+            A_sig_raw, A_bkg_raw = self.get_event_counts(selection=A_sel, raw=True)
+            B_sig_raw, B_bkg_raw = self.get_event_counts(selection=B_sel, raw=True)
+            C_sig_raw, C_bkg_raw = self.get_event_counts(selection=C_sel, raw=True)
+            D_sig_raw, D_bkg_raw = self.get_event_counts(selection=D_sel, raw=True)
+        if show_data:
+            if A_sig_wgt/A_bkg_wgt <= 0.2:
+                A_data = self.data_count(selection=A_sel)
+                A_data_err = np.sqrt(A_data)
+            else:
+                print("WARNING: Region A is signal polluted, blinding this region")
+                A_data = "BLINDED"
+                A_data_err = "BLINDED"
+            if B_sig_wgt/B_bkg_wgt <= 0.2:
+                B_data = self.data_count(selection=B_sel)
+                B_data_err = np.sqrt(B_data)
+            else:
+                print("WARNING: Region B is signal polluted, blinding this region")
+                B_data = "BLINDED"
+                B_data_err = "BLINDED"
+            if C_sig_wgt/C_bkg_wgt <= 0.2:
+                C_data = self.data_count(selection=C_sel)
+                C_data_err = np.sqrt(C_data)
+            else:
+                print("WARNING: Region C is signal polluted, blinding this region")
+                C_data = "BLINDED"
+                C_data_err = "BLINDED"
+
+        # Calculate extrapolation factors and relative errors
         AtoB_extp = B_bkg_wgt/A_bkg_wgt
         AtoB_err = np.sqrt((B_bkg_err/B_bkg_wgt)**2 + (A_bkg_err/A_bkg_wgt)**2)
         BtoC_extp = C_bkg_wgt/B_bkg_wgt
         BtoC_err = np.sqrt((C_bkg_err/C_bkg_wgt)**2 + (B_bkg_err/B_bkg_wgt)**2)
         CtoD_extp = D_bkg_wgt/C_bkg_wgt
         CtoD_err = np.sqrt((D_bkg_err/D_bkg_wgt)**2 + (C_bkg_err/C_bkg_wgt)**2)
-        # Print table
-        print("cut,region,bkg_raw,bkg_wgt,bkg_err,sig_raw,sig_wgt,sig_err")
-        print(f"{A_sel},A,{A_bkg_raw},{A_bkg_wgt},{A_bkg_err},{A_sig_raw},{A_sig_wgt},{A_sig_err}")
-        print(f"{B_sel},B,{B_bkg_raw},{B_bkg_wgt},{B_bkg_err},{B_sig_raw},{B_sig_wgt},{B_sig_err}")
-        print(f"{C_sel},C,{C_bkg_raw},{C_bkg_wgt},{C_bkg_err},{C_sig_raw},{C_sig_wgt},{C_sig_err}")
-        print(f"{D_sel},D,{D_bkg_raw},{D_bkg_wgt},{D_bkg_err},{D_sig_raw},{D_sig_wgt},{D_sig_err}")
+        # Construct tables
+        if show_raw:
+            header = "cut,region,bkg_raw,bkg_wgt,bkg_err,sig_raw,sig_wgt,sig_err"
+            A_row = f"{A_sel},A,{A_bkg_raw},{A_bkg_wgt},{A_bkg_err},{A_sig_raw},{A_sig_wgt},{A_sig_err}"
+            B_row = f"{B_sel},B,{B_bkg_raw},{B_bkg_wgt},{B_bkg_err},{B_sig_raw},{B_sig_wgt},{B_sig_err}"
+            C_row = f"{C_sel},C,{C_bkg_raw},{C_bkg_wgt},{C_bkg_err},{C_sig_raw},{C_sig_wgt},{C_sig_err}"
+            D_row = f"{D_sel},D,{D_bkg_raw},{D_bkg_wgt},{D_bkg_err},{D_sig_raw},{D_sig_wgt},{D_sig_err}"
+        else:
+            header = "cut,region,bkg_wgt,bkg_err,sig_wgt,sig_err"
+            A_row = f"{A_sel},A,{A_bkg_wgt},{A_bkg_err},{A_sig_wgt},{A_sig_err}"
+            B_row = f"{B_sel},B,{B_bkg_wgt},{B_bkg_err},{B_sig_wgt},{B_sig_err}"
+            C_row = f"{C_sel},C,{C_bkg_wgt},{C_bkg_err},{C_sig_wgt},{C_sig_err}"
+            D_row = f"{D_sel},D,{D_bkg_wgt},{D_bkg_err},{D_sig_wgt},{D_sig_err}"
+        if show_data:
+            header += ",data,data_err"
+            A_row += f",{A_data},{A_data_err}"
+            B_row += f",{B_data},{B_data_err}"
+            C_row += f",{C_data},{C_data_err}"
+            D_row += f",BLINDED,BLINDED"
+
+        # Print tables
+        print(header)
+        print(A_row)
+        print(B_row)
+        print(C_row)
+        print(D_row)
         print("")
         print("name,extp,rel_err")
         print(f"AtoB,{AtoB_extp},{AtoB_err}")
