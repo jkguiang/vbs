8ddc71a831b8b72a914ec4a528fd1b1b011d2d6b
Jonathan Guiang <jkguiang@gmail.com>
2022-10-17 15:14:31 -0700

    added AK8 JECs

--- analysis/studies/jes.h
+++ analysis/studies/jes.h
@@ -14,7 +14,8 @@
 struct JetEnergyScales
 {
 
-    JetCorrectionUncertainty* jec_unc;
+    JetCorrectionUncertainty* ak4_jec_unc;
+    JetCorrectionUncertainty* ak8_jec_unc;
     JetResolutionUncertainty* jer_unc;
     // Note: jec_var == 1 means the nominal value is applied, 
     //       +/-2 means a variation is applied, 
@@ -60,8 +61,11 @@ struct JetEnergyScales
     {
         // Init Jet Energy Correction (JEC) uncertainty scale factors
         // NOTE: must download them first!
-        jec_unc = new JetCorrectionUncertainty(
+        ak4_jec_unc = new JetCorrectionUncertainty(
             "NanoTools/NanoCORE/Tools/jetcorr/data/"+gconf.jecEraMC+"/"+gconf.jecEraMC+"_Uncertainty_AK4PFchs.txt"
+        );
+        ak8_jec_unc = new JetCorrectionUncertainty(
+            "NanoTools/NanoCORE/Tools/jetcorr/data/"+gconf.jecEraMC+"/"+gconf.jecEraMC+"_Uncertainty_AK8PFchs.txt"
         );
 
         // Init Jet Energy Resolution (JER) uncertainty scale factors
@@ -72,13 +76,22 @@ struct JetEnergyScales
         );
     };
 
-    LorentzVector applyJEC(LorentzVector jet_p4)
+    LorentzVector applyAK4JEC(LorentzVector jet_p4)
     {
         if (abs(jec_var) != 2) { return jet_p4; }
-        jec_unc->setJetEta(jet_p4.eta());
-        jec_unc->setJetPt(jet_p4.pt());
-        float jec_err = fabs(jec_unc->getUncertainty(jec_var == 2))*jec_var/2;
+        ak4_jec_unc->setJetEta(jet_p4.eta());
+        ak4_jec_unc->setJetPt(jet_p4.pt());
+        float jec_err = fabs(ak4_jec_unc->getUncertainty(jec_var == 2))*jec_var/2;
         return jet_p4*(1. + jec_err);
+    };
+
+    LorentzVector applyAK8JEC(LorentzVector fatjet_p4)
+    {
+        if (abs(jec_var) != 2) { return fatjet_p4; }
+        ak8_jec_unc->setJetEta(fatjet_p4.eta());
+        ak8_jec_unc->setJetPt(fatjet_p4.pt());
+        float jec_err = fabs(ak8_jec_unc->getUncertainty(jec_var == 2))*jec_var/2;
+        return fatjet_p4*(1. + jec_err);
     };
 
     LorentzVector applyJER(int seed, LorentzVector jet_p4, float rho, std::vector<LorentzVector> gen_jet_p4s)
