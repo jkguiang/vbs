1d335a21f2310ce08ae238563d2b0c651c10d0d4
Jonathan Guiang <jkguiang@gmail.com>
2023-02-16 13:59:18 -0800

    added VBSVVH skims

--- analysis/data/samples/skims.py
+++ analysis/data/samples/skims.py
@@ -12,3 +12,16 @@ bkg_1lep_1ak8_2ak4_v1 = [
 data_1lep_1ak8_2ak4_v1 = [
     DirectorySample(location=d, dataset=d.split("/")[-1].split("_data_1lep_1ak8")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVHSkim/data_1lep_1ak8_2ak4_v1/*")
 ]
+
+
+sig_0lep_2ak4_2ak8_ttH = [
+    DirectorySample(location=d, dataset=d.split("/")[-1].split("_sig_0lep_2ak4_2ak8_ttH")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVVHSkim/sig_0lep_2ak4_2ak8_ttH/*")
+]
+
+bkg_0lep_2ak4_2ak8_ttH = [
+    DirectorySample(location=d, dataset=d.split("/")[-1].split("_bkg_0lep_2ak4_2ak8_ttH")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVVHSkim/bkg_0lep_2ak4_2ak8_ttH/*")
+]
+
+data_0lep_2ak4_2ak8_ttH = [
+    DirectorySample(location=d, dataset=d.split("/")[-1].split("_data_0lep_2ak4_2ak8_ttH")[0], use_xrootd=True) for d in glob.glob("/ceph/cms/store/user/jguiang/VBSVVHSkim/data_0lep_2ak4_2ak8_ttH/*")
+]
