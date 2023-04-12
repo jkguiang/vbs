e4468561ed8a2d05cd947f17e7aec3020ce9086d
Jonathan Guiang <jkguiang@gmail.com>
2023-04-12 12:47:50 -0400

    added string representation for easy printing

--- abcdnet/utils.py
+++ abcdnet/utils.py
@@ -13,9 +13,24 @@ class VBSConfig(SimpleNamespace):
     def keys(self):
         return self.__dict__.keys()
 
+    def items(self):
+        return self.__dict__.items()
+
+    def __get_json(self, d):
+        json = {}
+        for key, val in d.items():
+            if type(val) == type(self):
+                json[key] = self.__get_json(val)
+            else:
+                json[key] = val
+        return json
+
+    def __str__(self):
+        return json.dumps(self.__get_json(self.__dict__), indent=4)
+
     def __getitem__(self, key):
         return self.get(key)
 
 def print_title(text):
     text = f" {text} "
-    print(f"{text:-^50}")
+    print(f"{text:-^50}", flush=True)
