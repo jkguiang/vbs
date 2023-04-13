a7ee371c3948cb100dd21988ddde5a2b932f7319
Jonathan Guiang <jkguiang@gmail.com>
2023-04-13 16:26:45 -0400

    added support for making a selection on the data before writing to .pt files

--- abcdnet/ingress.py
+++ abcdnet/ingress.py
@@ -33,7 +33,18 @@ def ingress(config):
     transforms = config.ingress.get("transforms", {})
     for file_i, root_file in enumerate(root_files):
         print(f"Loading {root_file}")
-        with uproot.open(f"{root_file}:{config.ingress.ttree_name}") as tree:
+        with uproot.open(root_file) as f:
+            # Collect branches to ingress
+            branches = (
+                config.ingress.features 
+                + config.ingress.get("label", []) 
+                + config.ingress.get("weights", [])
+            )
+            if config.ingress.get("disco_target", None):
+                branches.append(config.ingress.disco_target)
+
+            # Load TTree
+            tree = f[config.ingress.ttree_name].arrays(branches, cut=config.ingress.get("selection", None))
 
             # Assuming that files are named SampleName.root (e.g. QCD.root, ttbar1l.root, etc.)
             sample_name = root_file.split("/")[-1].replace(".root", "")
@@ -42,12 +53,12 @@ def ingress(config):
             # Load features
             features = []
             for feature_branch in config.ingress.features:
-                feature = torch.tensor(tree[feature_branch].array(), dtype=torch.float)
+                feature = torch.tensor(tree[feature_branch], dtype=torch.float)
                 feature = transform(feature, transforms.get(feature_branch, None))
                 features.append(feature)
 
             features = torch.transpose(torch.stack(features), 0, 1)
-            n_events = features.size()[0]
+            n_events = len(features)
 
             # Set label
             is_signal = False
@@ -58,7 +69,7 @@ def ingress(config):
                     is_signal = (root_file == config.ingress.signal_file)
                 labels = torch.ones(n_events)*is_signal
             elif config.ingress.get("label", None):
-                labels = torch.tensor(tree[config.ingress.label].array(), dtype=torch.float)
+                labels = torch.tensor(tree[config.ingress.label], dtype=torch.float)
             else:
                 raise Exception("No signal file name or label branch provided")
 
@@ -69,15 +80,12 @@ def ingress(config):
             weights = torch.ones(n_events)
             if config.ingress.get("weights", None):
                 for branch_i, weight_branch in enumerate(config.ingress.weights):
-                    weights *= torch.tensor(tree[weight_branch].array(), dtype=torch.float)
+                    weights *= torch.tensor(tree[weight_branch], dtype=torch.float)
 
             # Load disco target (to be used in SingleDisCo)
             disco_target = None
             if config.ingress.get("disco_target", None):
-                disco_target = torch.tensor(
-                    tree[config.ingress.disco_target].array(), 
-                    dtype=torch.float
-                )
+                disco_target = torch.tensor(tree[config.ingress.disco_target], dtype=torch.float)
                 disco_target = transform(disco_target, transforms.get(config.ingress.disco_target, None))
 
             # Save dataset
