65e71d7e52c8cf16d608b561ed3cad5e74bfd137
Jonathan Guiang <jkguiang@gmail.com>
2023-04-20 15:11:38 -0400

    updated

--- abcdnet/scripts/export.sh
+++ abcdnet/scripts/export.sh
@@ -1,16 +1,25 @@
-if [[ "$1" == "" ]]; then
-    echo "Usage: export.sh USER@HOST.EXT:/path/to/destination/dir"
+if [[ "$1" == "" || "$2" == "" ]]; then
+    echo "Usage: sh scripts/export.sh config/CONFIG.json USER@HOST.EXT:/path/to/destination/dir"
     exit 1
 fi
 mkdir -p tarballs
 
-INPUTDIR=$(cat config.json | grep -o '"input_dir": "[^"]*"' | sed 's/"input_dir": "//' | sed 's/"//' | awk '{print $1}')
-ABCDNAME=$(cat config.json | grep -o '"name": "[^"]*"' | sed 's/"name": "//' | sed 's/"//' | awk '{print $1}')
-ABCDNAME=$(echo $ABCDNAME | awk '{print $1}')
+INPUTDIR=$(cat $1 | grep -o '"input_dir": "[^"]*"' | sed 's/"input_dir": "//' | sed 's/"//' | awk '{print $1}')
+ABCDNAME=$(basename $1)
+ABCDNAME=${ABCDNAME%%.*}
+
+if [[ "$INPUTDIR" == "" ]]; then
+    echo "ERROR: encountered error when extracting input directory from $1"
+    exit 1
+fi
+if [[ "$ABCDNAME" == "" ]]; then
+    echo "ERROR: encountered error when extracting name from $1"
+    exit 1
+fi
 
 TARBALL=tarballs/${ABCDNAME}.tar.gz
 echo "Creating ${TARBALL}..."
 tar -zcvf $TARBALL $INPUTDIR/*_abcdnet.root $INPUTDIR/data.root
 echo "Done"
 
-scp $TARBALL $1
+scp $TARBALL $2
