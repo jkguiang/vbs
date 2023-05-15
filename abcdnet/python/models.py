3717623ee8d89f893fa1c6d1ff8ef28668c8bbae
Jonathan Guiang <jkguiang@gmail.com>
2023-05-15 11:50:55 -0400

    added NN with LeakyReLU activiation functions

--- abcdnet/python/models.py
+++ abcdnet/python/models.py
@@ -35,3 +35,35 @@ class NeuralNetwork(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+
+class LeakyNeuralNetwork(nn.Module):
+    def __init__(self, input_size, n_hidden_layers, hidden_size):
+        super().__init__()
+
+        hidden_layers = []
+        for layer_i in range(n_hidden_layers):
+            if layer_i == 0:
+                hidden_layers.append(nn.Linear(input_size, hidden_size))
+                hidden_layers.append(nn.LeakyReLU())
+            else:
+                hidden_layers.append(nn.Linear(hidden_size, hidden_size))
+                hidden_layers.append(nn.LeakyReLU())
+
+        self.layers = nn.Sequential(
+            *hidden_layers,
+            nn.Linear(hidden_size, 1),
+            nn.Sigmoid()
+        )
+
+        print_title("Neural Network")
+        print(self)
+
+    @classmethod
+    def from_config(cls, config):
+        input_size = len(config.ingress.features)
+        n_hidden_layers = config.model.n_hidden_layers
+        hidden_size = config.model.hidden_size
+        return cls(input_size, n_hidden_layers, hidden_size)
+
+    def forward(self, x):
+        return self.layers(x)
